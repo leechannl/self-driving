e6cbefc8a54c18d8aceca16011815427a30bb1ed
leechannl <leechannl@icloud.com>
2018-07-11 17:01:05 +0800

    add move function and test case

---
+++ localization_udacity_cs373.py
@@ -0,0 +1,98 @@
+# https://classroom.udacity.com/courses/cs373/lessons/48684821/concepts/487362110923
+
+# The function localize takes the following arguments:
+#
+# colors:
+#        2D list, each entry either 'R' (for red cell) or 'G' (for green cell)
+#
+# measurements:
+#        list of measurements taken by the robot, each entry either 'R' or 'G'
+#
+# motions:
+#        list of actions taken by the robot, each entry of the form [dy,dx],
+#        where dx refers to the change in the x-direction (positive meaning
+#        movement to the right) and dy refers to the change in the y-direction
+#        (positive meaning movement downward)
+#        NOTE: the *first* coordinate is change in y; the *second* coordinate is
+#              change in x
+#
+# sensor_right:
+#        float between 0 and 1, giving the probability that any given
+#        measurement is correct; the probability that the measurement is
+#        incorrect is 1-sensor_right
+#
+# p_move:
+#        float between 0 and 1, giving the probability that any given movement
+#        command takes place; the probability that the movement command fails
+#        (and the robot remains still) is 1-p_move; the robot will NOT overshoot
+#        its destination in this exercise
+#
+# The function should RETURN (not just show or print) a 2D list (of the same
+# dimensions as colors) that gives the probabilities that the robot occupies
+# each cell in the world.
+#
+# Compute the probabilities by assuming the robot initially has a uniform
+# probability of being in any cell.
+#
+# Also assume that at each step, the robot:
+# 1) first makes a movement,
+# 2) then takes a measurement.
+#
+# Motion:
+#  [0,0] - stay
+#  [0,1] - right
+#  [0,-1] - left
+#  [1,0] - down
+#  [-1,0] - up
+
+
+def localize(colors, measurements, motions, sensor_right, p_move):
+    # initializes p to a uniform distribution over a grid of the same dimensions as colors
+    p_init = 1.0 / float(len(colors)) / float(len(colors[0]))
+    p = [[p_init for _ in range(len(colors[0]))] for _ in range(len(colors))]
+
+    # >>> Insert your code here <<<
+
+    return p
+
+
+def sense_p():
+    pass
+
+
+def move(p, motion, p_move):
+    """
+    :param p: occurring probability in the world before movement, 2D list
+    :param motion: movement in list of 2 elements, (dy, dx)
+    :return: new occurring probability in the world after movement
+    """
+    q = [[] for _ in p]
+    for i in range(len(p)):
+        for j in range(len(p[i])):
+            s = p[(i - motion[0]) % len(p)][(j - motion[1]) % len(p[i])] * p_move
+            s += p[i][j] * (1 - p_move)
+            q[i].append(s)
+    return q
+
+
+def show(p):
+    rows = ['[' + ','.join(map(lambda x: '{0:.5f}'.format(x), r)) + ']' for r in p]
+    print '[' + ',\n '.join(rows) + ']'
+
+
+#############################################################
+# For the following test case, your output should be
+# [[0.01105, 0.02464, 0.06799, 0.04472, 0.02465],
+#  [0.00715, 0.01017, 0.08696, 0.07988, 0.00935],
+#  [0.00739, 0.00894, 0.11272, 0.35350, 0.04065],
+#  [0.00910, 0.00715, 0.01434, 0.04313, 0.03642]]
+# (within a tolerance of +/- 0.001 for each entry)
+
+colors = [['R', 'G', 'G', 'R', 'R'],
+          ['R', 'R', 'G', 'R', 'R'],
+          ['R', 'R', 'G', 'G', 'R'],
+          ['R', 'R', 'R', 'R', 'R']]
+measurements = ['G', 'G', 'G', 'G', 'G']
+motions = [[0, 0], [0, 1], [1, 0], [1, 0], [0, 1]]
+p = localize(colors, measurements, motions, sensor_right=0.7, p_move=0.8)
+show(p)  # displays your answer
---
+++ test_localization_udacity_cs373.py
@@ -0,0 +1,42 @@
+from localization_udacity_cs373 import move
+
+
+class TestMove(object):
+    colors = [['R', 'G', 'G', 'R', 'R'],
+              ['R', 'R', 'G', 'R', 'R'],
+              ['R', 'R', 'G', 'G', 'R'],
+              ['R', 'R', 'R', 'R', 'R']]
+    p_init = 1.0 / float(len(colors)) / float(len(colors[0]))
+    uniform_p = [[p_init for _ in range(len(colors[0]))] for _ in range(len(colors))]
+    nonuniform_p = [
+        [1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ]
+    nonuniform_p_after_move_right_by_1 = [
+        [0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ]
+    nonuniform_p_after_move_down_by_1 = [
+        [0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ]
+    move_right_by_1 = [0, 1]
+    move_down_by_1 = [1, 0]
+    p_exact_move = 1.0
+    p_inexact_move = 0.8
+
+    def test_uniform(self):
+        assert move(self.uniform_p, self.move_right_by_1, self.p_exact_move) == self.uniform_p
+
+    def test_nonuniform_move_right_by_1(self):
+        assert move(self.nonuniform_p, self.move_right_by_1,
+                    self.p_exact_move) == self.nonuniform_p_after_move_right_by_1
+
+    def test_nonuniform_move_down_by_1(self):
+        assert move(self.nonuniform_p, self.move_down_by_1, self.p_exact_move) == self.nonuniform_p_after_move_down_by_1
